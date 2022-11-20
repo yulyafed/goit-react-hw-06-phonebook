ce9764a55e27de564d18ee9ddcbb25bdf5c114fa
Yuliia Fedorychak <Yulyafed86@gmail.com>
2022-11-21 00:53:28 +0200

    add changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,61 +1,17 @@
-// import { nanoid } from 'nanoid';
-// import { useState, useEffect } from 'react';
 import { PhonebookForm } from './PhonebookForm/PhonebookForm';
 import { ContactsFilter } from './ContactsFilter/ContactsFilter';
 import { ContactsList } from './ContactsList/ContactsList';
 import { Title, TitleContact } from './App.styled';
 
-export default function App () {
+export default function App() {
   
-  // const [contacts, setContacts] = useState(() => {
-  //   return JSON.parse(localStorage.getItem('contacts')) ?? [];
-  // } );
-  // const [filter, setFilter] = useState('');
-  
-  // const filteredContacts = () => {
-  //   if (filter === '') {
-  //     return contacts;
-  //   }
-
-  //   return contacts.filter(item =>
-  //     item.name.toLocaleLowerCase().includes(filter)
-  //   );
-  // };
-
-  // const deleteContacts = contactId => { 
-  //   setContacts(contacts.filter(contact => contact.id !== contactId))
-  // }
-  
-  // useEffect(() => {
-     
-  //     localStorage.setItem('contacts', JSON.stringify(contacts));
-   
-  // },[contacts]);
-
-    return (
+      return (
       <>
         <Title>Phonebook</Title>
-        <PhonebookForm
-          // addContact={(contactName, contactNumber) => {
-          //   if (!contacts.some(contact => contact.name === contactName)) {
-          //     setContacts([
-          //       ...contacts,
-          //       { id: nanoid(), name: contactName, number: contactNumber },
-          //     ]);
-          //     return;
-          //   }
-          //   alert(`${contactName} is already in contacts`);
-          // }}
-        />
+        <PhonebookForm/>
         <TitleContact>Contacts</TitleContact>
-        <ContactsFilter
-          // initialValue={filter}
-          // filterChanged={filterValue => setFilter(filterValue)}
-        />
-        <ContactsList
-          // contacts={filteredContacts()}
-          // onDeleteContact={deleteContacts}
-        />
+        <ContactsFilter/>
+        <ContactsList/>
       </>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Provider } from "react-redux";
 import ReactDOM from 'react-dom/client';
 import { PersistGate } from 'redux-persist/integration/react';
-import App from 'components/App';
 import { store, persistor } from "./redux/store";
+import App from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
